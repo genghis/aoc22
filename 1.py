a8857a95f9858f0c6f30066863e2d996517e41a2
Genghis Philip <gphilip@nexben.com>
2022-11-30 23:50:31 -0600

    ugly but works

---
+++ 1.py
@@ -0,0 +1,32 @@
+input = open("input.txt","r").read()
+
+def one():
+    elves_raw = [i for i in input.split("\n")]
+    elves = []
+    temp = 0
+    for i in elves_raw:
+        if i == "":
+            elves.append(temp)
+            temp = 0
+        else:
+            temp+=int(i)
+    print(max(elves))
+
+def two():
+    elves_raw = [i for i in input.split("\n")]
+    elves = []
+    temp = 0
+    for i in elves_raw:
+        if i == "":
+            elves.append(temp)
+            temp = 0
+        else:   
+            temp+=int(i)
+    elves.sort(reverse = True)
+    print(sum(elves[0:3]))
+
+if __name__ == "__main__":
+    print("First:\n")
+    one()
+    print("\n\nSecond:\n")
+    two()
